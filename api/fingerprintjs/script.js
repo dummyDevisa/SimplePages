c261da6b64bdd2808751fa57c747a9669a0a7256
dummyDevisa <141086212+dummyDevisa@users.noreply.github.com>
2024-10-02 09:19:16 -0300

    Create script.js
    Fetch a fingerprint browser

---
+++ api/fingerprintjs/script.js
@@ -0,0 +1,16 @@
+document.getElementById('get-fingerprint').addEventListener('click', () => {
+    const fpPromise = import('https://openfpcdn.io/fingerprintjs/v4')
+        .then(FingerprintJS => FingerprintJS.load());
+
+    fpPromise
+        .then(fp => fp.get())
+        .then(result => {
+            const visitorId = result.visitorId;
+            console.log(visitorId);
+            document.getElementById('result').textContent = JSON.stringify({ visitorId: visitorId }, null, 2);
+        })
+        .catch(err => {
+            console.error(err);
+            document.getElementById('result').textContent = JSON.stringify({ error: err.message }, null, 2);
+        });
+});
